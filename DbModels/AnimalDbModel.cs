3222b69943f8936b21cf892b710c678cedb5dad9
Ilyas <ilyas.ali@softwire.com>
2021-07-19 12:32:55 +0100

    Initial commit + Zoo contexs

---
+++ DbModels/AnimalDbModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zoo.DbModels
+{
+    public class AnimalDbModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfArrival { get; set; }
+        public string Classification { get; set; }
+        public string Sex { get; set; }
+        public string Species { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Zoo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public static readonly ILoggerFactory
+            LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        private static string CORS_POLICY_NAME = "_myfaceCorsPolicy";
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MyFaceDbContext>(options =>
+            {
+                options.UseLoggerFactory(LoggerFactory);
+                options.UseSqlite("Data Source=myface.db");
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CORS_POLICY_NAME, builder =>
+                {
+                    builder
+                        .WithOrigins("http://localhost:3000")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Zoo", Version = "v1" });
+            });
+
+            services.AddControllers();
+            //add transients
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zoo v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
